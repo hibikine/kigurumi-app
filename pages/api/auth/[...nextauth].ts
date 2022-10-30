7e623f6db7073d1b011b4ed2f28158a98d807e91
Hibikine Kage <goodsmile314159@gmail.com>
2022-10-30 14:09:11 +0900

    add auth to nextauth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,15 +1,67 @@
 import NextAuth from 'next-auth';
+import EmailProvider from 'next-auth/providers/email';
 import TwitterProvider from 'next-auth/providers/twitter';
-if (!process.env.TWITTER_CLIENT_ID || !process.env.TWITTER_CLIENT_SECRET) {
-  throw new Error('Missing Twitter environment variables');
+import GoogleProvider from 'next-auth/providers/google';
+import { Client as FaunaClient } from 'faunadb';
+import { FaunaAdapter } from '@next-auth/fauna-adapter';
+if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
+  throw new Error('Missing Google environment variables');
 }
+if (
+  !process.env.EMAIL_SERVER_USER ||
+  !process.env.EMAIL_SERVER_PASSWORD ||
+  !process.env.EMAIL_SERVER_HOST ||
+  !process.env.EMAIL_SERVER_PORT ||
+  !process.env.EMAIL_FROM
+) {
+  throw new Error('Missing email environment variables');
+}
+if (
+  !process.env.FAUNADB_SECRET ||
+  !process.env.FAUNADB_SCHEME ||
+  !process.env.FAUNADB_PORT
+) {
+  throw new Error('Missing FaunaDB environment variables');
+}
+const client = new FaunaClient({
+  secret: process.env.FAUNADB_SECRET,
+  scheme: process.env.FAUNADB_SCHEME as 'http' | 'https',
+  domain: process.env.FAUNADB_DOMAIN,
+  port: parseInt(process.env.FAUNADB_PORT, 10),
+});
 export const authOptions = {
   providers: [
     TwitterProvider({
-      clientId: process.env.TWITTER_CLIENT_ID ?? '',
-      clientSecret: process.env.TWITTER_CLIENT_SECRET ?? '',
+      clientId: process.env.TWITTER_CLIENT_ID,
+      clientSecret: process.env.TWITTER_CLIENT_SECRET,
       version: '2.0',
     }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    }),
   ],
+  /*adapter: FirestoreAdapter({
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  }),*/
+  adapter: FaunaAdapter(client),
 };
 export default NextAuth(authOptions);
