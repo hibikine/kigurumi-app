087f88b5d3e75f0ae2e2bb84c9e6ceedc741628e
Hibikine Kage <goodsmile314159@gmail.com>
2022-10-31 10:22:22 +0900

    fix typing

--- utils/database.ts
+++ utils/database.ts
@@ -1,8 +1,16 @@
 import { Client as FaunaClient } from 'faunadb';
+if (
+  !process.env.FAUNA_SECRET ||
+  !process.env.FAUNA_SCHEME ||
+  !process.env.FAUNA_DOMAIN ||
+  !process.env.FAUNA_PORT
+) {
+  throw new Error('Missing environment variables');
+}
 export const getFaunaClient = () =>
   new FaunaClient({
-    secret: process.env.FAUNADB_SECRET,
-    scheme: process.env.FAUNADB_SCHEME as 'http' | 'https',
-    domain: process.env.FAUNADB_DOMAIN,
+    secret: process.env.FAUNADB_SECRET!,
+    scheme: process.env.FAUNADB_SCHEME! as 'http' | 'https',
+    domain: process.env.FAUNADB_DOMAIN!,
     port: parseInt(process.env.FAUNADB_PORT || '', 10),
   });
