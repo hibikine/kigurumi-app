c1e79f10723556481bb2c063272245b40478ac49
Hibikine Kage <goodsmile314159@gmail.com>
2022-11-14 16:09:21 +0900

    fix(auth): :bug: use according cookie name on checking a user session
    On server, use according cookie name of next auth session by http or https.

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,1 +1,2 @@
-export const SESSION_TOKEN_COOKIE = '__Secure-next-auth.session-token';
+export const SESSION_TOKEN_COOKIE_HTTPS = '__Secure-next-auth.session-token';
+export const SESSION_TOKEN_COOKIE_HTTP = 'next-auth.session-token';
--- src/utils/session.ts
+++ src/utils/session.ts
@@ -1,10 +1,19 @@
 import { IncomingMessage } from 'http';
 import { GetServerSideProps } from 'next';
-import { SESSION_TOKEN_COOKIE } from './constants';
+import {
+  SESSION_TOKEN_COOKIE_HTTP,
+  SESSION_TOKEN_COOKIE_HTTPS,
+} from './constants';
 import { JSONCookie } from 'cookie-parser';
 
-export const getSessionToken = (req: IncomingMessage): string | null =>
-  ((req as any)?.cookies ??
-    (req?.headers?.cookie && JSONCookie(req.headers.cookie)))?.[
-    SESSION_TOKEN_COOKIE
-  ] || null;
+export const getSessionToken = (req: IncomingMessage): string | null => {
+  return (
+    ((req as any)?.cookies ??
+      (req?.headers?.cookie && JSONCookie(req.headers.cookie)))?.[
+      req.headers['x-forwarded-proto'] === 'https' ||
+      process.env.HTTPS === 'true'
+        ? SESSION_TOKEN_COOKIE_HTTPS
+        : SESSION_TOKEN_COOKIE_HTTP
+    ] || null
+  );
+};
