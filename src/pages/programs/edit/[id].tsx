2d4a196d7c08b68dee65de8bef2c824b9d4dba0c
Hibikine Kage <goodsmile314159@gmail.com>
2022-12-22 15:21:54 +0000

    fix: :bug: add spinner, fix dayjs path, remove unnecesary codes, fix text

--- src/pages/programs/edit/[id].tsx
+++ src/pages/programs/edit/[id].tsx
@@ -10,6 +10,7 @@ import {
   Heading,
   Textarea,
   TextareaField,
+  Spinner,
 } from 'evergreen-ui';
 import Link from 'next/link';
 import type { NextPage } from 'next';
@@ -24,12 +25,7 @@ import {
 } from '../../../generated/request';
 import { useCallback, createRef, useState } from 'react';
 import { useRouter } from 'next/router';
-import dayjs from 'dayjs';
-import UTC from 'dayjs/plugin/utc';
-import Timezone from 'dayjs/plugin/timezone';
-
-dayjs.extend(Timezone);
-dayjs.extend(UTC);
+import dayjs from '../../../lib/dayjs';
 
 type FormData = {
   name: string;
@@ -58,7 +54,7 @@ const Belongings: NextPage = () => {
     setValue,
     formState: { errors },
   } = useForm<FormData>();
-  const { data, refetch } = useProgramQuery(
+  const { data, refetch, isSuccess } = useProgramQuery(
     { id: parseInt(id as string, 10) },
     {
       enabled: !!id,
@@ -67,7 +63,8 @@ const Belongings: NextPage = () => {
         setValue(
           'date',
           data?.program?.date
-            ? dayjs(data.program.date)
+            ? dayjs
+                .tz(data.program.date, 'UTC')
                 .tz('Asia/Tokyo')
                 .format('YYYY-MM-DDTHH:mm')
             : ''
@@ -75,7 +72,8 @@ const Belongings: NextPage = () => {
         setValue(
           'endDate',
           data?.program?.endDate
-            ? dayjs(data.program.endDate)
+            ? dayjs
+                .tz(data.program.endDate, 'UTC')
                 .tz('Asia/Tokyo')
                 .format('YYYY-MM-DDTHH:mm')
             : ''
@@ -96,16 +94,15 @@ const Belongings: NextPage = () => {
       console.log('a');
       await updateProgramMutation.mutateAsync({
         ...data,
-        date: dayjs.tz(data.date, 'Asia/Tokyo').toISOString(),
+        date: dayjs.tz(data.date, 'Asia/Tokyo').utc().toISOString(),
         endDate: data.endDate
-          ? dayjs.tz(data.endDate, 'Asia/Tokyo').toISOString()
+          ? dayjs.tz(data.endDate, 'Asia/Tokyo').utc().toISOString()
           : data.endDate,
         id: parseInt(id as string, 10),
       });
     } catch (e) {
       throw e;
     }
-    console.log('updated');
     router.push(`/programs/item/${id}`);
   });
 
@@ -118,82 +115,93 @@ const Belongings: NextPage = () => {
       <div className="flex flex-col items-center">
         <Pane className="flex flex-col items-left max-w-2xl mt-8 mb-8 w-full">
           <Heading marginBottom={majorScale(2)} size={700} is="h1">
-            合わせを追加する
+            合わせを編集する
           </Heading>
-          <form onSubmit={onSubmit}>
-            <TextInputField
-              label="イベント名"
-              description="合わせの名前を入力してください。"
-              placeholder="○○合わせ"
-              isRequired
-              {...register('name', { required: true })}
-            />
-            <FormField
-              marginBottom={24}
-              label="開始日時"
-              isRequired
-              labelFor="date"
-            >
-              <input
-                className="text-slate-900"
-                type="datetime-local"
-                id="date"
-                {...register('date', { required: true })}
+          {isSuccess ? (
+            <form onSubmit={onSubmit}>
+              <TextInputField
+                label="イベント名"
+                description="合わせの名前を入力してください。"
+                placeholder="○○合わせ"
+                isRequired
+                {...register('name', { required: true })}
               />
-            </FormField>
-            <FormField marginBottom={24} label="終了日時" labelFor="endDate">
-              <input
-                className="text-slate-900"
-                type="datetime-local"
-                id="endDate"
-                {...register('endDate')}
-              />
-              <Button
-                type="button"
-                marginLeft={majorScale(2)}
-                size="small"
-                onClick={() => reset({ endDate: undefined })}
+              <FormField
+                marginBottom={24}
+                label="開始日時"
+                isRequired
+                labelFor="date"
               >
-                終了日時を設定しない
-              </Button>
-            </FormField>
-            <TextInputField
-              label="開催場所"
-              description="集合場所や撮影場所を記入してください。"
-              placeholder="中庭"
-              {...register('location')}
-            />
-            <TextInputField
-              label="合わせのURL"
-              description="TwiplaやツイートのURLを入力してください。"
-              placeholder="https://twipla.jp/events/000000"
-              type="url"
-              {...register('url')}
-            />
-            <TextInputField
-              label="主催者のURL"
-              description="TwitterなどのURLを入力してください。"
-              placeholder="https://twitter.com/hikageworks"
-              type="url"
-              {...register('ownerUrl')}
-            />
-            <TextareaField
-              label="イベント詳細"
-              {...register('detail')}
-              inputHeight={200}
-            />
-            <div className="mb-4">
-              {/*<ReCAPTCHA
+                <input
+                  className="text-slate-900"
+                  type="datetime-local"
+                  id="date"
+                  {...register('date', { required: true })}
+                />
+              </FormField>
+              <FormField marginBottom={24} label="終了日時" labelFor="endDate">
+                <input
+                  className="text-slate-900"
+                  type="datetime-local"
+                  id="endDate"
+                  {...register('endDate')}
+                />
+                <Button
+                  type="button"
+                  marginLeft={majorScale(2)}
+                  size="small"
+                  onClick={() => reset({ endDate: undefined })}
+                >
+                  終了日時を設定しない
+                </Button>
+              </FormField>
+              <TextInputField
+                label="開催場所"
+                description="集合場所や撮影場所を記入してください。"
+                placeholder="中庭"
+                {...register('location')}
+              />
+              <TextInputField
+                label="合わせのURL"
+                description="TwiplaやツイートのURLを入力してください。"
+                placeholder="https://twipla.jp/events/000000"
+                type="url"
+                {...register('url')}
+              />
+              <TextInputField
+                label="主催者のURL"
+                description="TwitterなどのURLを入力してください。"
+                placeholder="https://twitter.com/hikageworks"
+                type="url"
+                {...register('ownerUrl')}
+              />
+              <TextareaField
+                label="イベント詳細"
+                {...register('detail')}
+                inputHeight={200}
+              />
+              <div className="mb-4">
+                {/*<ReCAPTCHA
                 onChange={() => setRecaptchaSuccess(true)}
                 onErrored={() => setRecaptchaSuccess(false)}
                 onExpired={() => setRecaptchaSuccess(false)}
                 sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY!}
   />*/}
+              </div>
+              <Button
+                className="w-full"
+                appearance="primary"
+                alignSelf="center"
+                display="block"
+              >
+                更新
+              </Button>
+            </form>
+          ) : (
+            <div className="w-full h-screen flex justify-center items-center">
+              <Spinner />
             </div>
-            <Button appearance="primary" alignSelf="center" display="block">
-              更新
-            </Button>
-          </form>
+          )}
         </Pane>
       </div>
     </Layout>
