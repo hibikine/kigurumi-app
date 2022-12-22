1783fa707b7933c8bc42ceba17fc869052b97173
Hibikine Kage <goodsmile314159@gmail.com>
2022-12-22 15:31:27 +0000

    feat: :speech_balloon: add footer text

--- src/components/PageFooter.tsx
+++ src/components/PageFooter.tsx
@@ -1,4 +1,16 @@
+import Link from 'next/link';
+
 const PageFooter = () => {
-  return null;
+  return (
+    <small className="text-slate-500 text-center w-full block mt-auto mb-1">
+      &copy; 2022{' '}
+      <Link
+        className="underline hover:text-slate-600"
+        href="https://twitter.com/hikageworks"
+      >
+        Hikage Works
+      </Link>
+    </small>
+  );
 };
 export default PageFooter;
