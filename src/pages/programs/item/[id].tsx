3979f4a9bdd4ee19a4fe3a786cce1ad44221daa5
Hibikine Kage <goodsmile314159@gmail.com>
2022-12-22 15:24:48 +0000

    fix: :bug: fix ui, fix dayjs, add loading spinner

--- src/pages/programs/item/[id].tsx
+++ src/pages/programs/item/[id].tsx
@@ -9,14 +9,12 @@ import {
 } from '../../../generated/request';
 import EventUrl from '../../../components/EventUrl';
 import nl2br from 'react-nl2br';
-import dayjs from 'dayjs';
-import 'dayjs/locale/ja';
+import dayjs from '../../../lib/dayjs';
 import { Button, majorScale, Spinner, TextInputField } from 'evergreen-ui';
 import { KABadge } from '../../../components/KABadge';
 import { useState, Fragment, useCallback } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
 import { TrashIcon } from '@heroicons/react/20/solid';
-dayjs.locale('ja');
 
 const Loading = () => {
   return (
@@ -29,7 +27,7 @@ const Loading = () => {
 const ProgramsItem: NextPage = () => {
   const router = useRouter();
   const { id } = router.query;
-  const { data, refetch, isFetched } = useProgramQuery({
+  const { data, refetch, isFetched, isLoadingError } = useProgramQuery({
     id: parseInt(id as string, 10),
   });
   const deleteProgramMutation = useDeleteProgramMutation();
@@ -43,19 +41,20 @@ const ProgramsItem: NextPage = () => {
     [setIsOpenDeleteModal]
   );
   const [startDateReverse, setStartDateReverse] = useState('');
-  if (data === undefined) {
-    return null;
-  }
-  if (data.program == null) {
+  if (isLoadingError) {
     return <div>Not Found</div>;
   }
-  const { name, date, url, ownerUrl, detail, location } = data.program;
+  const { name, date, url, ownerUrl, detail, location } = data?.program ?? {};
   const calendarUrl = new URL('https://www.google.com/calendar/event');
   calendarUrl.searchParams.append('action', 'TEMPLATE');
-  calendarUrl.searchParams.append('text', name);
+  if (name) {
+    calendarUrl.searchParams.append('text', name);
+  }
   calendarUrl.searchParams.append(
     'dates',
-    `${dayjs(date).format('YYYYMMDDTHHmmss')}/${dayjs(date)
+    `${dayjs.tz(date, 'UTC').tz('Asia/Tokyo').format('YYYYMMDDTHHmmss')}/${dayjs
+      .tz(date, 'UTC')
+      .tz('Asia/Tokyo')
       .add(1, 'hour')
       .format('YYYYMMDDTHHmmss')}`
   );
@@ -69,9 +68,9 @@ const ProgramsItem: NextPage = () => {
 
   return (
     <Layout>
-      {isFetched ? (
+      {isFetched && data ? (
         <>
-          <div className="flex flex-col items-center pl-2 pr-2">
+          <div className="flex flex-col items-center pl-2 pr-2 mb-10">
             <div className="w-full lg:w-96 max-w-full text-slate-700 ">
               <div className="self-start pt-2 pb-2">
                 <Link className=" text-blue-500 " href="/programs">
@@ -81,8 +80,12 @@ const ProgramsItem: NextPage = () => {
               <div className="bg-white pl-4 pr-4 pt-4 pb-8 rounded-xl flex flex-col">
                 <h1 className="text-2xl font-bold">{name}</h1>
                 <div className="mt-2 flex justify-between align-center">
-                  <KABadge size="large">
-                    {dayjs(date).format('D日(dd) HH:mm')}～
+                  <KABadge size="small">
+                    {dayjs
+                      .tz(date, 'UTC')
+                      .tz('Asia/Tokyo')
+                      .format('D日(dd) HH:mm')}
+                    ～
                   </KABadge>
                   <Link legacyBehavior passHref href={calendarUrl}>
                     <Button is="a" size="small" marginRight={majorScale(1)}>
@@ -116,20 +119,18 @@ const ProgramsItem: NextPage = () => {
                 )}
                 <div className="border-t mt-6 mb-6" />
                 <div className="flex justify-end">
-                  <Link legacyBehavior passHref href={`/programs/edit/${id}`}>
-                    <Button is="a" size="small">
-                      編集する
-                    </Button>
+                  <Link
+                    className="border border-slate-400 text-2xs flex justify-center items-center rounded px-2 py-0.5"
+                    href={`/programs/edit/${id}`}
+                  >
+                    編集する
                   </Link>
-                  <Button
-                    className="ml-4"
-                    is="a"
-                    size="small"
-                    intent="danger"
+                  <button
+                    className="ml-4 border border-red-400 text-red-500 text-2xs flex justify-center items-center rounded px-2 py-0.5"
                     onClick={openModal}
                   >
                     削除する
-                  </Button>
+                  </button>
                 </div>
               </div>
             </div>
@@ -181,7 +182,10 @@ const ProgramsItem: NextPage = () => {
                         <p className="mt-4">
                           開始日時:{' '}
                           <span className="font-bold">
-                            {dayjs(date).format('YYYY-MM-DD HH:mm')}
+                            {dayjs
+                              .tz(date, 'UTC')
+                              .tz('Asia/Tokyo')
+                              .format('YYYY-MM-DD HH:mm')}
                           </span>
                         </p>
                         <div className="mt-2">
@@ -203,13 +207,17 @@ const ProgramsItem: NextPage = () => {
                           appearance="primary"
                           intent="danger"
                           disabled={
-                            dayjs(date).format('mm:HH DD-MM-YYYY') !==
-                            startDateReverse
+                            dayjs
+                              .tz(date, 'UTC')
+                              .tz('Asia/Tokyo')
+                              .format('mm:HH DD-MM-YYYY') !== startDateReverse
                           }
                           onClick={async () => {
                             if (
-                              dayjs(date).format('mm:HH DD-MM-YYYY') !==
-                              startDateReverse
+                              dayjs
+                                .tz(date, 'UTC')
+                                .tz('Asia/Tokyo')
+                                .format('mm:HH DD-MM-YYYY') !== startDateReverse
                             ) {
                               return;
                             }
